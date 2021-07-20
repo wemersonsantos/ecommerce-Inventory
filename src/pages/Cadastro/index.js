e5b3b55536670abc3d8da5ea82ea1b7fd29facb2
wemersonsantos <wemersonsantoseng@gmail.com>
2021-07-20 16:22:54 -0300

    Estrutura do cadastro do usuario finalizado

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -2,26 +2,84 @@ import React, {useState} from 'react';
 
 export default function Cadastro(props) {
 
-        const [post, setPost] = useState({})
+        const usersRegister = {
+            fullName: '',
+            email: '',
+            phone: '',
+            CPF_CNPJ:'',
+            RG:'',
+            Sex:''
+        }
+
+        const usersAdress = {
+            adress: '',
+            district: '',
+            complement: '',
+            city:'',
+            CEP:'',
+            UF:'',
+            country:''
+        }
+
+        const [users, setUsers] = useState(usersRegister)
+        const [adress, setAdress] = useState(usersAdress)
 
         const submit = (e) => {
-
             e.preventDefault();
-            console.log(post)
-            
+            console.log(users);   
+            console.log(adress);  
           }
 
     return(
         <>
+            <h3>Cadastro Cliente</h3>
+
             <form onSubmit={submit}>
-                <label>Title: </label>
-                    <input type="text" name="title" onChange={e => setPost({...post, title: e.target.value})}/>
-                    <br />
-                    <label>Post Content: </label>
-                    <textarea name="content" onChange={e => setPost({...post, content: e.target.value})}>
-                    </textarea>
-                    <br />
-                    <input type="submit"/>
+                <fieldset>
+                    <legend className='titleForm'>Dados do Usuário</legend>
+                    <label>Nome Completo</label>
+                    <input type='text' id='name' name='fullName' className='formCadastro' onChange={e => setUsers({...users, fullName: e.target.value})} />
+    
+                    <label>Email</label>
+                    <input type='email' id='email' name='email' className='formCadastro' onChange={e => setUsers({...users, email: e.target.value})}/>
+    
+                    <label>Telefone(DDD)</label>
+                    <input type='number' id='phone' name='phone' className='formCadastro' onChange={e => setUsers({...users, phone: e.target.value})}/>
+    
+                    <label>CPF/CNPJ</label>
+                    <input type='number' id='register' name='register' className='formCadastro' onChange={e => setUsers({...users, CPF_CNPJ: e.target.value})}/>
+    
+                    <label>RG</label>
+                    <input type='number' id='rg' name='rg' className='formCadastro' onChange={e => setUsers({...users, RG: e.target.value})}/>
+    
+                    <label>Sexo</label>
+                    <input type='radio' id='Masc' name='sex' className='formCadastro' onChange={e => setUsers({...users, Sex: e.target.value})}/><label>Masculino</label>
+                    <input type='radio' id='Fem' name='sex' className='formCadastro' onChange={e => setUsers({...users, Sex: e.target.value})}/><label>Feminino</label>
+                </fieldset>
+                <fieldset>
+                    <legend className='titleForm'>Endereço do Usuário</legend>
+                    <label>Endereço</label>
+                    <input type='text' id='address' name='address' className='formCadastro' onChange={e => setAdress({...adress, adress: e.target.value})}/>
+
+                    <label>Bairro</label>
+                    <input type='text' id='district' name='district' className='formCadastro' onChange={e => setAdress({...adress, district: e.target.value})}/>
+
+                    <label>Complemento</label>
+                    <input type='text' id='complement' name='complement' className='formCadastro' onChange={e => setAdress({...adress, complement: e.target.value})}/>
+    
+                    <label>Cidade</label>
+                    <input type='text' id='city' name='city' className='formCadastro' onChange={e => setAdress({...adress, city: e.target.value})}/>
+
+                    <label>CEP</label>
+                    <input type='number' id='cep' name='cep' className='formCadastro' onChange={e => setAdress({...adress, CEP: e.target.value})}/>
+    
+                    <label>UF</label>
+                    <input type='text' id='register' name='register' className='formCadastro' onChange={e => setAdress({...adress, UF: e.target.value})}/>
+
+                    <label>Pais</label>
+                    <input type='text' id='country' name='country' className='formCadastro' onChange={e => setAdress({...adress, country: e.target.value})}/>
+                </fieldset>
+            <button type='submit' >Cadastrar</button>
             </form>  
         </>
     )
