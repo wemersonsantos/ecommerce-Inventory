e4aa59c3724453fe4233fccd00d5b698722f85fd
wemersonsantos <wemersonsantoseng@gmail.com>
2021-07-24 20:09:16 -0300

    server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:9000/produtos'
+    baseURL: 'https://my-json-server.typicode.com/wemersonsantos/ecommerce-Inventory'
 })
 
 export default api
